1f13060de3e1f06ef94f0ed40d62d6d317d5f9ab
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-28 21:32:03 -0300

    Primeira versão da aula de coleções

---
+++ src/Teste01.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Teste01 {
+    public static void main(String[] args) {
+
+        List<Aluno> lista = new ArrayList<>();
+        /* lista.add(new Aluno(85000, "Renato Nivaldex", "SI"));
+        lista.add(new Aluno(15000, "Ismael Ligeirinho", "ADM"));
+        lista.add(new Aluno(35000, "Patricia Arvore Binaria", "Engenharia"));
+        lista.add(new Aluno(10000, "Ana Maria", "SI"));
+        lista.add(new Aluno(20000, "Ana Maria", "ADM")); */
+
+        lista.forEach(aluno -> System.out.println(aluno));
+
+        lista.sort(Comparator.comparing(Aluno::getNome));
+
+        System.out.println();
+
+        lista.forEach(aluno -> System.out.println(aluno));
+
+        lista.sort(Comparator.comparing(Aluno::getNome).reversed());
+    }
+}
